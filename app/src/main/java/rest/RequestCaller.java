d2b4654c14475496c9c41056633406ea8e8758ac
Alejandro Velasco <savelascod@unal.edu.co>
2015-11-19 18:54:31 -0500

    -Changed rest get Client request Caller

--- app/src/main/java/rest/RequestCaller.java
+++ app/src/main/java/rest/RequestCaller.java
@@ -2,24 +2,21 @@ package rest;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.HttpParams;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by mordreth on 10/2/15.
  */
 public class RequestCaller {
-    String hostUrl = "https://okylifeapi.herokuapp.com";
+    String hostUrl = "http://servicedatosabiertoscolombia.cloudapp.net";
     HttpParams parameters;
 
-    public void getById(String classUriPath, long id, AsyncResponse responseHandler) {
-        String url = this.hostUrl + "/" + classUriPath + "/show";
-        List<NameValuePair> parameters = new ArrayList<NameValuePair>();
-        parameters.add(new BasicNameValuePair("id", String.valueOf(id)));
-        url += "?" + URLEncodedUtils.format(parameters, "ISO-8859-1");
+    public void getData(String classUriPath, AsyncResponse responseHandler, List<NameValuePair> params) {
+        String paramString = URLEncodedUtils.format(params, "utf-8");
+        String url = this.hostUrl + "/" + classUriPath;
+        url += "?" + paramString;
         GetClient getClient = new GetClient(url, responseHandler);
         getClient.execute();
     }
